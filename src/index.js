24381ee7448b6c71d798cff360787a9a6abea4a0
Andrey4ik222 <107884250+Andrey4ik222@users.noreply.github.com>
2023-05-18 18:34:34 +0200

    использовал createHashRouter из-за GitHub pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createHashRouter,
+} from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Albums from "./pages/Albums/Albums";
 import Photos from "./pages/Photos/Photos";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
